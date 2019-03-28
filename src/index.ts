754ebec1c82e715324a6c26ad806a473176f1ecd
Federico Pinna <fpinna@vivocha.com>
2019-03-28 16:46:25 +0100

    fix: re-export all symbols from jsonref

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { SchemaOptions } from './global';
 import { Schema, StaticSchema } from './schema';
 
+export * from 'jsonref';
 export { SchemaError, SchemaOptions, ValidationError, ValidationOptions } from './global';
 export { Schema, StaticSchema } from './schema';
+
 
 export function create(dataOrUri:any, opts:SchemaOptions): Promise<Schema> {
   return StaticSchema.create(dataOrUri, opts);
