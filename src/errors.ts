9ada4f070cd69f58f78ae41358d60434713e879c
Federico Pinna <fpinna@vivocha.com>
2020-09-02 10:38:24 +0200

    feat: add method to ValidationError to get (recursive) error info

--- src/errors.ts
+++ src/errors.ts
@@ -4,10 +4,33 @@ export class SchemaError extends Error {
     this.name = 'SchemaError';
   }
 }
+
+export interface ValidationErrorInfo {
+  type: string;
+  path?: string;
+  scope?: string;
+  errors?: ValidationErrorInfo[];
+}
+
 export class ValidationError extends Error {
   constructor(public path: string, public scope: string, type: string, public errors?: Error[]) {
     super(type);
     this.name = 'ValidationError';
     if (!this.path) this.path = '/';
+  }
+  getInfo(): ValidationErrorInfo {
+    return ValidationError.getInfo(this);
+  }
+  static getInfo(err: Error): ValidationErrorInfo {
+    const out: ValidationErrorInfo = { type: err.message };
+    if (err.name === 'ValidationError') {
+      const verr: ValidationError = err as ValidationError;
+      out.path = verr.path;
+      out.scope = verr.scope;
+      if (verr.errors) {
+        out.errors = verr.errors.map((e) => ValidationError.getInfo(e));
+      }
+    }
+    return out;
   }
 }
